ef96df4a73be1a3a195db2b4207f7c6879838737
HeilAsuka <heilasuka911@gmail.com>
2022-06-18 13:10:49 +0800

    Add UDP support

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 # Sigil
 
-A TCP forwarding tool   
-To proxy all tcp traffic on localhost port 8080 to remote host 10.0.1.100 port 5900 you can run:   
+A Port forwarding tool   
+To proxy all tcp or udp traffic on localhost port 8080 to remote host 10.0.1.100 port 5900 you can run:   
 ```sigil -l 127.0.0.1:8080 -r 10.0.1.100:5900```   
 This crate use [tokio](https://github.com/tokio-rs/tokio) as async runtime.
 
 # TODO
 
-support UDP
+-[x] support UDP
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use anyhow::{Result, Ok};
+use anyhow::{Ok, Result};
 use structopt::StructOpt;
 use tokio::{
     io::AsyncWriteExt,
-    net::{TcpListener, TcpStream, ToSocketAddrs},
+    net::{TcpListener, TcpStream, ToSocketAddrs, UdpSocket},
 };
 
 #[derive(StructOpt, Debug)]
@@ -29,6 +29,8 @@ struct Cli {
     // threads: usize,
 }
 
+const UDP_BUFFER_SIZE: usize = 2048;
+
 fn parse_cli() -> Cli {
     Cli::from_args_safe().unwrap_or_else(|e| {
         println!("{}", e.message);
@@ -41,6 +43,7 @@ async fn main() -> Result<()> {
     let remote_addr = args.remote.to_owned();
     let local_addr = args.local.to_owned();
     start_tcp_forwarding(&local_addr, &remote_addr).await?;
+    start_udp_forwarding(&local_addr, &remote_addr).await?;
     Ok(())
 }
 
@@ -49,7 +52,7 @@ async fn start_tcp_forwarding(localaddr: &str, remote_addr: &str) -> Result<()> 
     loop {
         let (incoming, _) = locallistener.accept().await?;
         let remote_addr = remote_addr.to_owned();
-        tokio::spawn(async move{
+        tokio::spawn(async move {
             if let Err(e) = tcp_forwarding(incoming, remote_addr).await {
                 println!("{}", e);
             };
@@ -73,4 +76,51 @@ async fn tcp_forwarding<S: ToSocketAddrs>(mut incoming: TcpStream, remote_addr: 
     };
     tokio::try_join!(incoming_to_outgoing, outgoing_to_incoming)?;
     Ok(())
+}
+
+async fn start_udp_forwarding(localaddr: &str, remote_addr: &str) -> Result<()> {
+    let mut local_socket = UdpSocket::bind(localaddr).await?;
+    let remote_addr = remote_addr.to_owned();
+    tokio::spawn(async move {
+        match udp_forwarding(&mut local_socket, remote_addr).await {
+            anyhow::Result::Ok(_) => (),
+            Err(e) => println!("{}", e),
+        }
+    });
+    Ok(())
+}
+
+async fn udp_forwarding(local_socket: &mut UdpSocket, remote_addr: String) -> Result<()> {
+    let mut remote_socket = UdpSocket::bind("0.0.0.0:0").await?;
+    remote_socket.connect(remote_addr).await?;
+    loop {
+        match handle_incoming_package(local_socket, &mut remote_socket).await {
+            Result::Ok(_) => (),
+            Err(e) => println!("{}", e),
+        }
+    }
+}
+
+async fn handle_incoming_package(
+    local_socket: &mut UdpSocket,
+    remote_socket: &mut UdpSocket,
+) -> Result<()> {
+    let mut buf = [0u8; UDP_BUFFER_SIZE];
+    let (amount, client_addr) = local_socket.recv_from(&mut buf).await?;
+    let payload = &buf[..amount];
+    remote_socket.send(payload).await?;
+    match remote_socket.try_recv_from(&mut buf) {
+        anyhow::Result::Ok((amount, _)) => {
+            let payload = &buf[..amount];
+            local_socket.send_to(payload, client_addr).await?;
+            Ok(())
+        }
+        Err(e) => {
+            if e.kind() == std::io::ErrorKind::WouldBlock {
+                Ok(())
+            } else {
+                Err(e.into())
+            }
+        }
+    }
 }
