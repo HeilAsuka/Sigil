cc227683c5d895fa4249109ec16b691a8fd7b363
HeilAsuka <heilasuka911@gmail.com>
2022-09-27 18:08:12 +0800

    fix some bug

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,32 @@
-use anyhow::{Result, Ok};
-use structopt::StructOpt;
-use tokio::{
-    io::AsyncWriteExt,
-    net::{TcpListener, TcpStream, ToSocketAddrs},
-};
+use std::net::SocketAddr;
 
-#[derive(StructOpt, Debug)]
-#[structopt(name = "Sigil", about = "A TCP forwarding tool.")]
+use anyhow::Result;
+use clap::Parser;
+
+use tokio::io::copy_bidirectional;
+use tokio::net::{TcpListener, TcpStream, ToSocketAddrs, UdpSocket};
+
 /// A TCP forwarding tool
 ///
 /// To proxy all tcp traffic on localhost port 8080 to remote host 10.0.1.100 port 5900 you can run:
 ///
 /// sigil -l 127.0.0.1:8080 -r 10.0.1.100:5900
-struct Cli {
-    #[structopt(short = "l", long = "local", help = "Local address and port")]
+#[derive(Parser)]
+#[clap(name = "sigil")]
+#[clap(author = "HeilAsuka <heilasuka911@gmail.com>")]
+#[clap(version = "0.1.0")]
+struct Args {
     /// Specify the listening address i.e. 127.0.0.1:8080
+    #[clap(short, long)]
     local: String,
-    #[structopt(short = "r", long = "remote", help = "Remote address and port")]
     /// Specify the remote address to proxy to i.e. 10.0.1.100:5900
+    #[clap(short, long)]
     remote: String,
-    // #[structopt(
-    //     short = "t",
-    //     long = "threads",
-    //     help = "Number of threads to use",
-    //     default_value = "4"
-    // )]
-    // /// Specify the number of threads to use, default value is 4
-    // threads: usize,
 }
 
-fn parse_cli() -> Cli {
-    Cli::from_args_safe().unwrap_or_else(|e| {
-        println!("{}", e.message);
+fn parse_cli() -> Args {
+    Args::try_parse().unwrap_or_else(|e: clap::Error| {
+        println!("{}", e);
         e.exit();
     })
 }
@@ -40,16 +35,18 @@ async fn main() -> Result<()> {
     let args = parse_cli();
     let remote_addr = args.remote.to_owned();
     let local_addr = args.local.to_owned();
-    start_tcp_forwarding(&local_addr, &remote_addr).await?;
+    let _a = tokio::join!(
+        start_tcp_forwarding(&local_addr, &remote_addr),
+    );
     Ok(())
 }
 
-async fn start_tcp_forwarding(localaddr: &str, remote_addr: &str) -> Result<()> {
-    let locallistener = TcpListener::bind(localaddr).await?;
+async fn start_tcp_forwarding(local_addr: &str, remote_addr: &str) -> Result<()> {
+    let locallistener = TcpListener::bind(local_addr).await?;
     loop {
         let (incoming, _) = locallistener.accept().await?;
         let remote_addr = remote_addr.to_owned();
-        tokio::spawn(async move{
+        tokio::spawn(async move {
             if let Err(e) = tcp_forwarding(incoming, remote_addr).await {
                 println!("{}", e);
             };
@@ -59,18 +56,6 @@ async fn start_tcp_forwarding(localaddr: &str, remote_addr: &str) -> Result<()> 
 
 async fn tcp_forwarding<S: ToSocketAddrs>(mut incoming: TcpStream, remote_addr: S) -> Result<()> {
     let mut outgoing = TcpStream::connect(remote_addr).await?;
-    let (mut ri, mut wi) = incoming.split();
-    let (mut ro, mut wo) = outgoing.split();
-    let incoming_to_outgoing = async {
-        tokio::io::copy(&mut ri, &mut wo).await?;
-        wo.shutdown().await?;
-        Ok(())
-    };
-    let outgoing_to_incoming = async {
-        tokio::io::copy(&mut ro, &mut wi).await?;
-        wi.shutdown().await?;
-        Ok(())
-    };
-    tokio::try_join!(incoming_to_outgoing, outgoing_to_incoming)?;
+    copy_bidirectional(&mut incoming, &mut outgoing).await?;
     Ok(())
 }
